e1cbfddc5fe858c353c74c04505cff69f13078b4
praseesh <praseeshprasee@gmail.com>
2024-02-21 08:58:01 +0530

    some changes

--- voting/views.py
+++ voting/views.py
@@ -8,14 +8,14 @@ def eligibility(request):
     age=int(request.POST['age1'])
     print(age)
     if age < 18:
-        return render(request,'ineligible.html',({'eligibility': age}))
+        return render(request,'ineligible.html')
     else:
-        return render(request,'eligibile.html',({'eligibility': age}))
+        return render(request,'eligibile.html')
 
 
         
 
 
-# (venv) P:\DJango\Entri\Eligibility>cd..\venv\scripts&&deactivate
+# (venv) P:\DJango\Entri\Eligibility>cd..\venv\scripts&&activate
 # P:\DJango\Entri\venv\Scripts>activate
 # (venv) P:\DJango\Entri\venv\Scripts>cd..\..\Eligibility
