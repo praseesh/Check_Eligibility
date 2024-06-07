c1408c9c3752fee4c22a3a9b79a57779b85200e4
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-06-07 23:11:25 +0530

    Update settings.py

--- Eligibility/settings.py
+++ Eligibility/settings.py
@@ -18,6 +18,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
